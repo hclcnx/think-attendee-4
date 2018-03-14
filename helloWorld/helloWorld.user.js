4f12ecca786360175d8db3316fca7417a7a3df5a
thinkattendee14 <37315440+thinkattendee14@users.noreply.github.com>
2018-03-14 16:16:13 +0000

    Update helloWorld.user.js

--- helloWorld/helloWorld.user.js
+++ helloWorld/helloWorld.user.js
@@ -45,7 +45,7 @@ if(typeof(dojo) != "undefined") {
             waitFor( function(){
 			// wait until the "loading..." node has been hidden
 			// indicating that we have loaded content.
-   			dojo.query("span.shareSome-title")[0].textContent="Hello From Holland " + lconn.homepage.global.beans + "! ";
+   			dojo.query("span.shareSome-title")[0].textContent="Hello From Holland " + lconn.homepage.username + "! ";
        	          },
 		  ".lotusStreamTopLoading div.loaderMain.lotusHidden");
       } catch(e) {
